6ef3c705d8e40de3b06abeea581200327bc70277
natinimn <natinimn@microsoft.com>
2017-05-23 15:25:21 -0700

    update RegenerateClient script

--- Sample/cs/Recommendations.Client/RegenerateClient.cmd
+++ Sample/cs/Recommendations.Client/RegenerateClient.cmd
@@ -1,3 +1,5 @@
+@ECHO off
+
 REM ******************************************
 REM
 REM Copyright (c) Microsoft Corporation. All rights reserved.
@@ -7,13 +9,10 @@ REM Regenerates the Recommendations client
 REM
 REM ******************************************
 
-@echo off
+
 
 REM download the most up-to-date swagger file or use a URL
 REM SET swaggerLocation = swagger.json
 SET swaggerLocation=https://localhost:44342/swagger/docs/v1
 
 ..\packages\AutoRest.0.17.3\tools\AutoRest.exe -Input %swaggerLocation% -Namespace Recommendations.Client -ModelsName Entities
-
-
-
